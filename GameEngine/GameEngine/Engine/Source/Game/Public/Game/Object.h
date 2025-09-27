3e93cccfecc2c6567ff0e932dad7912e24c6392c
bixboy <pierrelamare62@gmail.com>
2025-09-27 16:18:21 +0200

    oui oui

--- GameEngine/GameEngine/Engine/Source/Game/Public/Game/Object.h
+++ GameEngine/GameEngine/Engine/Source/Game/Public/Game/Object.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <iosfwd>
 #include <string>
 #include <string_view>
+#include <istream>   // 👈 nécessaire pour std::istream::read
+#include <ostream>   // 👈 nécessaire pour std::ostream::write
 
 #include "Math/Math.h"
 
@@ -16,41 +17,12 @@ namespace Engine::Game
         Object(std::string name, const Math::Transform& transform);
         virtual ~Object() = default;
 
-        [[nodiscard]] std::string_view GetUUID() const noexcept { return uuid_; }
-        void SetUUID(std::string uuid);
-
-        [[nodiscard]] std::string_view GetName() const noexcept { return name_; }
-        void SetName(std::string name);
-
-        [[nodiscard]] virtual std::string_view GetTypeName() const noexcept;
-
-        [[nodiscard]] const Math::Transform& GetTransform() const noexcept { return transform_; }
-        void SetTransform(const Math::Transform& transform) noexcept { transform_ = transform; }
-
-        [[nodiscard]] Math::Vector3 GetPosition() const noexcept { return transform_.position; }
-        void SetPosition(const Math::Vector3& position) noexcept { transform_.position = position; }
-        void SetPositionX(float x) noexcept { transform_.position.x = x; }
-        void SetPositionY(float y) noexcept { transform_.position.y = y; }
-        void SetPositionZ(float z) noexcept { transform_.position.z = z; }
-
-        [[nodiscard]] Math::Rotator GetRotation() const noexcept { return transform_.rotation; }
-        void SetRotation(const Math::Rotator& rotation) noexcept { transform_.rotation = rotation; }
-        void SetRotationPitch(float pitch) noexcept { transform_.rotation.pitch = pitch; }
-        void SetRotationYaw(float yaw) noexcept { transform_.rotation.yaw = yaw; }
-        void SetRotationRoll(float roll) noexcept { transform_.rotation.roll = roll; }
-
-        [[nodiscard]] Math::Vector3 GetScale() const noexcept { return transform_.scale; }
-        void SetScale(const Math::Vector3& scale) noexcept { transform_.scale = scale; }
-        void SetScaleX(float x) noexcept { transform_.scale.x = x; }
-        void SetScaleY(float y) noexcept { transform_.scale.y = y; }
-        void SetScaleZ(float z) noexcept { transform_.scale.z = z; }
+        // ... (le reste de ton code identique)
 
         virtual void SerializeBinary(std::ostream& stream) const;
         virtual void DeserializeBinary(std::istream& stream);
 
     protected:
-        [[nodiscard]] Math::Transform& GetMutableTransform() noexcept { return transform_; }
-
         virtual void SerializeBinaryImpl(std::ostream& stream) const {}
         virtual void DeserializeBinaryImpl(std::istream& stream) {}
 
