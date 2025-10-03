d9a088dd1fed0ca55846c0d43a9ab79d78660a83
bixboy <56200627+bixboy@users.noreply.github.com>
2025-10-03 20:33:38 +0200

    Remove action entries from content tooltips

--- GameEngine/GameEngine/Engine/Source/Engine/Private/Gui/Widgets/ActionTooltip.cpp
+++ GameEngine/GameEngine/Engine/Source/Engine/Private/Gui/Widgets/ActionTooltip.cpp
@@ -4,32 +4,6 @@
 
 namespace Engine::Gui
 {
-    namespace
-    {
-        void DrawActionButtons(const ActionTooltipAction* actions, std::size_t count)
-        {
-            ImGui::Indent(8.0f);
-            for (std::size_t index = 0; index < count; ++index)
-            {
-                const ActionTooltipAction& action = actions[index];
-                if (action.Label.IsEmpty())
-                    continue;
-
-                ImGui::PushID(static_cast<int>(index));
-                ImGui::TextDisabled("%s", action.Label.c_str());
-
-                if (ImGui::IsItemHovered())
-                    ImGui::SetMouseCursor(ImGuiMouseCursor_Hand);
-
-                if (action.Callback && ImGui::IsItemClicked())
-                    action.Callback();
-
-                ImGui::PopID();
-            }
-            ImGui::Unindent(8.0f);
-        }
-    }
-
     void ShowActionTooltip(const String& header, std::initializer_list<ActionTooltipAction> actions)
     {
         ShowActionTooltip(header, actions.begin(), actions.size());
@@ -40,24 +14,11 @@ namespace Engine::Gui
         if (!ImGui::BeginTooltip())
             return;
 
-        const bool hasHeader = !header.IsEmpty();
-        if (hasHeader)
-        {
-            const auto headerView = header.View();
-            ImGui::TextUnformatted(headerView.data(), headerView.data() + headerView.size());
-        }
+        (void)actions;
+        (void)count;
 
-        if (count > 0)
-        {
-            if (hasHeader)
-                ImGui::Separator();
-
-            DrawActionButtons(actions, count);
-        }
-        else if (hasHeader)
-        {
-            ImGui::TextDisabled("Aucune action disponible.");
-        }
+        const auto headerView = header.View();
+        ImGui::TextUnformatted(headerView.data(), headerView.data() + headerView.size());
 
         ImGui::EndTooltip();
     }
